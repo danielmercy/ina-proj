69177dbb6105515434228f12dc86f7f220f54a2c
daniel mercy <dayodanielmercy@gmail.com>
2019-03-22 10:26:04 +0100

    Added statistics, profile and settings pages to the dashboard module

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [StatisticsComponent, ProfileComponent, SettingsComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule
