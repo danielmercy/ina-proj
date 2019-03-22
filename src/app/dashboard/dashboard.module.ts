b14cc2c504e862f764f637721a77eb53d6e51190
daniel mercy <dayodanielmercy@gmail.com>
2019-03-22 11:08:10 +0100

    Added a manager component to the dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,9 +5,10 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { StatisticsComponent } from './statistics/statistics.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SettingsComponent } from './settings/settings.component';
+import { ManagerComponent } from './manager/manager.component';
 
 @NgModule({
-  declarations: [StatisticsComponent, ProfileComponent, SettingsComponent],
+  declarations: [StatisticsComponent, ProfileComponent, SettingsComponent, ManagerComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule
